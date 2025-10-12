53bd45ba990d6fb8e8a57939a63d976c667f8dff
ErnestoAGEV <vevelerne@gmail.com>
2025-10-12 14:07:10 -0600

    Implement sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import './App.css'
-import { QueryClient } from '@tanstack/react-query';
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { MyRoutes } from "./routers/router";
+import { useThemeStore } from "./store/ThemeStore";
 
 function App() {
+  const {theme} = useThemeStore();
+  document.documentElement.classList.toggle("dark",theme === "dark")
   const queryClient = new QueryClient();
 
   return (
     <QueryClientProvider client={queryClient}>
-
-       <ReactQueryDevtools initialIsOpen={false} />
+      <MyRoutes />
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,71 @@
+import { Icon } from "@iconify/react";
+import { NavLink } from "react-router-dom";
+import { BtnToggleTheme } from "./ui/buttons/BtnToggleTheme";
+
+export const Sidebar = () => {
+  const links = [
+    {
+      label: "Inicio",
+      icon: "ic:baseline-home",
+      to: "/",
+    },
+    {
+      label: "Notificaciones",
+      icon: "ic:baseline-notifications",
+      to: "/notificaciones",
+    },
+    {
+      label: "Mensajes",
+      icon: "ic:baseline-message",
+      to: "/mensajes",
+    },
+    {
+      label: "Colecciones",
+      icon: "ic:baseline-collections-bookmark",
+      to: "/colecciones",
+    },
+    {
+      label: "Suscripciones",
+      icon: "ic:baseline-person",
+      to: "/suscripciones",
+    },
+    {
+      label: "Añadir Tarjeta",
+      icon: "ic:baseline-credit-card",
+      to: "/tarjeta",
+    },
+    {
+      label: "Mi Perfil",
+      icon: "ic:baseline-account-circle",
+      to: "/miperfil",
+    },
+  ];
+  return (
+    <div className="h-screen p-2 bg-white dark:bg-bg-dark transition-all duration-300">
+      {/*LOGO*/}
+      <div>ONLYDEVS</div>
+      {/*NAV*/}
+      <nav className="flex-1 flex flex-col gap-2 items-center">
+        {links.map((item, index) => {
+          return (
+            <NavLink
+              key={index}
+              to={item.to}
+              className={({ isActive }) =>
+                `flex items-center gap-3 p-2 rounded-lg font-semibold hover:bg-gray-100 dark:hover:bg-primary/10 dark:hover:text-primary transition-all w-full justify-center justify-start ${
+                  isActive
+                    ? "text-blue-600 dark:text-white"
+                    : "text-gray-600 dark:text-gray-400"
+                }`
+              }
+            >
+              <Icon icon={item.icon} width={24} height={24} />
+              <span>{item.label}</span>
+            </NavLink>
+          );
+        })}
+      </nav>
+      <BtnToggleTheme/>
+    </div>
+  );
+};
